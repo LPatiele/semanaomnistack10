95121d0112a5853c2295ff88e121c44dbddb46bf
Lorraine <lorraine_patiele@uft.edu.br>
2020-01-16 14:50:52 -0300

    inicializando web

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+   
+      <header className="App-header">
+       aaa
+      </header>
+  );
+}
+
+export default App;
